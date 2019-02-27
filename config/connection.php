a68c5c391c92b902ca9d0bba01f5db8bff470288
vbpawar <vikaspawar3110@gmail.com>
2019-02-27 11:52:51 +0530

    vikas

--- config/connection.php
+++ config/connection.php
@@ -3,7 +3,6 @@ $servername = 'localhost';
 $username   = 'root';
 $password   = '';
 $dbname ='khajana26';
-
+echo 'vikas';
 $con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error());
-
 ?>
