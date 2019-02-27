4af2291965b2d9c3642fb3e209af55574fb7af37
vbpawar <vikaspawar3110@gmail.com>
2019-02-27 11:58:11 +0530

    removing messags

--- config/connection.php
+++ config/connection.php
@@ -3,6 +3,5 @@ $servername = 'localhost';
 $username   = 'root';
 $password   = '';
 $dbname ='khajana26';
-echo 'vikas';
 $con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error());
 ?>
